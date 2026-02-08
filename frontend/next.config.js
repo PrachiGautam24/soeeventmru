aa3723675a67c10215ae52d5a7c7ed078dfa7125
harshb14 <soeevents@mru.edu.in>
2026-02-09 01:11:02 +0530

    .

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'ui-avatars.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 }
